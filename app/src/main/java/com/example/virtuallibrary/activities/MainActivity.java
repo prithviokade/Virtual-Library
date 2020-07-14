bf0408aa9350fd1fba06e4f7ebe12e0ebb437e34
Prithvi Okade <prithviro@fb.com>
2020-07-14 11:18:29 -0400

    add button navigation

--- app/src/main/java/com/example/virtuallibrary/activities/MainActivity.java
+++ app/src/main/java/com/example/virtuallibrary/activities/MainActivity.java
@@ -7,9 +7,16 @@ import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.virtuallibrary.R;
+import com.example.virtuallibrary.fragments.CreateGoalFragment;
+import com.example.virtuallibrary.fragments.CreatePostFragment;
+import com.example.virtuallibrary.fragments.CreateTableFragment;
 import com.example.virtuallibrary.fragments.GoalsFragment;
 import com.example.virtuallibrary.fragments.HomeFragment;
 import com.example.virtuallibrary.fragments.PostsFragment;
@@ -18,15 +25,18 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    public String fragTag = "HomeFragment";
 
     BottomNavigationView bottomNavigation;
     final FragmentManager fragmentManager = getSupportFragmentManager();
+    ImageButton btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnAdd = findViewById(R.id.btnAdd);
         bottomNavigation = findViewById(R.id.bottomNavigation);
         // getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         // getSupportActionBar().setCustomView(R.layout.actionbar);
@@ -41,12 +51,15 @@ public class MainActivity extends AppCompatActivity {
                 switch (menuItem.getItemId()) {
                     case R.id.actionHome:
                     default:
+                        fragTag = "HomeFragment";
                         fragment =  new HomeFragment();
                         break;
                     case R.id.actionProgress:
+                        fragTag = "GoalsFragment";
                         fragment = new GoalsFragment();
                         break;
                     case R.id.actionPosts:
+                        fragTag = "PostsFragment";
                         // getSupportActionBar().hide();
                         fragment = new PostsFragment();
                         break;
@@ -57,6 +70,28 @@ public class MainActivity extends AppCompatActivity {
         });
         // Set default selection
         bottomNavigation.setSelectedItemId(R.id.actionHome);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Fragment fragment;
+                // case on each fragment and navigate to the corresponding add table/goal/post fragment
+                switch (fragTag) {
+                    case "HomeFragment":
+                    default:
+                        fragment = new CreateTableFragment();
+                        break;
+                    case "GoalsFragment":
+                        fragment = new CreateGoalFragment();
+                        break;
+                    case "PostsFragment":
+                        fragment = new CreatePostFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                btnAdd.setVisibility(View.INVISIBLE);
+            }
+        });
 
     }
 
