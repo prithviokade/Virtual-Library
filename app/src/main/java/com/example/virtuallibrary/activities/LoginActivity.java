fec52b9fedc3fb675ddbca1f1a63ff4e965b66d5
Prithvi Okade <prithviro@fb.com>
2020-07-16 01:00:57 -0400

    Facebook login

--- app/src/main/java/com/example/virtuallibrary/activities/LoginActivity.java
+++ app/src/main/java/com/example/virtuallibrary/activities/LoginActivity.java
@@ -13,9 +13,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.virtuallibrary.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
+
+import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -24,11 +32,53 @@ public class LoginActivity extends AppCompatActivity {
     EditText etPassword;
     Button btnLogin;
     TextView tvSignUp;
+    private static final String EMAIL = "email";
+    private static final String USER_POSTS = "user_posts";
+    private static final String AUTH_TYPE = "rerequest";
+
+    private CallbackManager mCallbackManager;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mCallbackManager = CallbackManager.Factory.create();
+
+        LoginButton mLoginButton = findViewById(R.id.login_button);
+
+        // Set the initial permissions to request from the user while logging in
+        mLoginButton.setPermissions(Arrays.asList(EMAIL, USER_POSTS));
+
+        mLoginButton.setAuthType(AUTH_TYPE);
+
+        // Register a callback to respond to the user
+        mLoginButton.registerCallback(
+                mCallbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        setResult(RESULT_OK);
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        setResult(RESULT_CANCELED);
+                        finish();
+                    }
+
+                    @Override
+                    public void onError(FacebookException e) {
+                        Log.e(TAG, "help", e);
+                    }
+                });
 
         if (ParseUser.getCurrentUser() != null) {
             goMainActivity();
